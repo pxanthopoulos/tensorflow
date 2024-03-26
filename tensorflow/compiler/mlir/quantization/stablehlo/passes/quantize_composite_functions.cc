920c08d88d799f93f3b8a368565b4d32289ab178
A. Unique TensorFlower <gardener@tensorflow.org>
2024-03-26 16:14:19 -0700

    Implement a pass to merge requantize+dequantize for StableHLO Quantizer
    This pass will convert req+dq pattern in the quantized fusion to dq+relu(6).
    
    Also add an option in PipelineConfig to enable this pass. This option is defaulted to false for ODML pipeline. But enabled by server.
    
    Reverts e5c11f3d1e1bef39cb1ea0937ac5addf04942f32
    
    PiperOrigin-RevId: 619342925

--- tensorflow/compiler/mlir/quantization/stablehlo/passes/quantize_composite_functions.cc
+++ tensorflow/compiler/mlir/quantization/stablehlo/passes/quantize_composite_functions.cc
@@ -98,6 +98,11 @@ void QuantizeCompositeFunctionsPass::runOnOperation() {
   pm.addPass(createQuantizePass(quantize_options));
   pm.addNestedPass<func::FuncOp>(createPostQuantizePass());
 
+  // TODO: b/321729008 - move this implementation to quantization_patterns.cc.
+  if (merge_fusion_with_dequantize_) {
+    pm.addPass(createMergeFusionWithDequantizePass());
+  }
+
   ModuleOp module_op = getOperation();
   if (const absl::Status pm_run_status =
           RunPassesOnModuleOp(mlir_dump_file_name_, pm, module_op);
